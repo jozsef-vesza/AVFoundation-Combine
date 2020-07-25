00a5a1a874537941ea0ebe6b7ecb1b1ca969f8db
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2020-07-25 11:21:46 +0200

    Adds isPlaybackBufferEmptyPublisher() Adds more comments

--- AVFoundation-Combine/AVPlayerItem+Publishers.swift
+++ AVFoundation-Combine/AVPlayerItem+Publishers.swift
@@ -13,13 +13,23 @@ import AVKit
 public extension AVPlayerItem {
     
     /// Publisher for the `isPlaybackLikelyToKeepUp` property.
+    /// A Boolean value that indicates whether the item will likely play through without stalling.
     /// - Returns: Publisher for the `isPlaybackLikelyToKeepUp` property.
     func isPlaybackLikelyToKeepUpPublisher() -> Publishers.PlayerItemIsPlaybackLikelyToKeepUpPublisher {
         let keyPath: KeyPath<AVPlayerItem, Bool> = \.isPlaybackLikelyToKeepUp
         return Publishers.KVObservingPublisher(observedObject: self, keyPath: keyPath)
     }
     
+    /// Publisher for the `isPlaybackBufferEmpty` property.
+    /// A Boolean value that indicates whether playback has consumed all buffered media and that playback will stall or end.
+    /// - Returns: Publisher for the `isPlaybackBufferEmpty` property.
+    func isPlaybackBufferEmptyPublisher() -> AnyPublisher<Bool, Never> {
+        let keyPath: KeyPath<AVPlayerItem, Bool> = \.isPlaybackBufferEmpty
+        return Publishers.KVObservingPublisher(observedObject: self, keyPath: keyPath).eraseToAnyPublisher()
+    }
+    
     /// Publisher for the `status` property.
+    /// The status of the player item.
     /// - Returns: Publisher for the `status` property.
     func statusPublisher() -> Publishers.PlayerItemStatusPublisher {
         let keyPath: KeyPath<AVPlayerItem, AVPlayerItem.Status> = \.status
