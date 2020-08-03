a2fc39ba095d0fa55bd5ddf547d52713d47f0e85
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2020-08-03 17:11:49 +0200

    Removes `statusPublisher()`, `isPlaybackLikelyToKeepUpPublisher()` and `isPlaybackBufferEmptyPublisher()` from `AVPlayer` Publisher extensions

--- AVFoundation-Combine/AVPlayer+Publishers.swift
+++ AVFoundation-Combine/AVPlayer+Publishers.swift
@@ -33,36 +33,4 @@ public extension AVPlayer {
     func currentItemPublisher() -> AnyPublisher<AVPlayerItem?, Never> {
          publisher(for: \.currentItem).eraseToAnyPublisher()
     }
-    
-    // MARK: AVPlayerItem Publishers
-    
-    /// Publisher for the `status` property in `AVPlayer.currentItem`
-    /// A status that indicates whether the player can be used for playback.
-    /// - Returns: Publisher for the `status` property in `AVPlayer.currentItem`
-    func statusPublisher() -> AnyPublisher<AVPlayerItem.Status, Never> {
-        guard let currentItem = currentItem else {
-            return Empty().eraseToAnyPublisher()
-        }
-        return currentItem.statusPublisher()
-    }
-    
-    /// Publisher for the `isPlaybackLikelyToKeepUp` property in `AVPlayer.currentItem`
-    /// A Boolean value that indicates whether the item will likely play through without stalling.
-    /// - Returns: Publisher for the `isPlaybackLikelyToKeepUp` property in `AVPlayer.currentItem`
-    func isPlaybackLikelyToKeepUpPublisher() -> AnyPublisher<Bool, Never> {
-        guard let currentItem = currentItem else {
-            return Empty().eraseToAnyPublisher()
-        }
-        return currentItem.isPlaybackLikelyToKeepUpPublisher()
-    }
-    
-    /// Publisher for the `isPlaybackBufferEmpty` property.
-    /// A Boolean value that indicates whether playback has consumed all buffered media and that playback will stall or end.
-    /// - Returns: Publisher for the `isPlaybackBufferEmpty` property.
-    func isPlaybackBufferEmptyPublisher() -> AnyPublisher<Bool, Never> {
-        guard let currentItem = currentItem else {
-            return Empty().eraseToAnyPublisher()
-        }
-        return currentItem.isPlaybackBufferEmptyPublisher()
-    }
 }
