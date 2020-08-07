ee4e0a211378d81c0c4729adf4a4bf588bd1efb1
József Vesza <jozsef.vesza@outlook.com>
2020-08-07 15:15:50 +0200

    Prevent loading the default scene when unit testing

--- AVFoundation-Combine/Supporting Files/SceneDelegate.swift
+++ AVFoundation-Combine/Supporting Files/SceneDelegate.swift
@@ -12,12 +12,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    var isUnitTesting: Bool {
+        return ProcessInfo.processInfo.arguments.contains("-UNITTEST")
+    }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard !isUnitTesting,   
+            let windowScene = (scene as? UIWindowScene),
+            let rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
