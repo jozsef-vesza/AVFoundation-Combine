7eab5b9f24c2130da2d6c565ab686eff2296e99c
Juan Carlos Ospina Gonzalez <piterwilson@gmail.com>
2021-04-30 15:43:34 +0200

    feat: Adds `func commonMetadataPublisher<T>(key: AVMetadataKey, as: T.Type) -> AnyPublisher<T, Never>`

--- Sources/AVFoundationCombine/Extensions/AVAsset+commonMetadata.swift
+++ Sources/AVFoundationCombine/Extensions/AVAsset+commonMetadata.swift
@@ -23,7 +23,7 @@ public extension AVAsset {
          }
          .store(in: &subscriptions)
      ```
-     - Parameter key: common key of the `AVMetadataItem`
+     - Parameter key: key of the `AVMetadataItem`
      - Returns: A `Publisher` that emits a specific `AVMetadataItem` with a given `AVMetadataKey` (if it exists in the `AVAsset`'s `commonMetadata` entries).
      */
     func commonMetadataPublisher(key: AVMetadataKey) -> AnyPublisher<AVMetadataItem, Never> {
@@ -47,13 +47,40 @@ public extension AVAsset {
          }
          .store(in: &subscriptions)
      ```
-     - Parameter key: common key of the `AVMetadataItem`
+     - Parameter key: key of the `AVMetadataItem`
      - Returns: A `Publisher` that emits the extras of a specific `AVMetadataItem` matching a given `AVMetadataKey` (if it exists in the `AVAsset`'s `commonMetadata` entries).
      */
     func commonMetadataExtrasPublisher(key: AVMetadataKey) -> AnyPublisher<[AVMetadataExtraAttributeKey: Any], Never> {
         publisher(for: \.commonMetadata)
             .compactMap { metadata -> [AVMetadataExtraAttributeKey: Any]? in
                 metadata.filter { $0.commonKey == key }.first?.extraAttributes
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    /**
+     `Publisher` that emits the value `AVMetadataItem` matching a given `AVMetadataKey` (if it exists in the `AVAsset`'s `commonMetadata` entries) casting said value to a given concrete type.
+     
+      This will only succeed if the  `AVMetadataItem` matching a given `AVMetadataKey` exists and casting to the concrete value succeeds.
+     
+     The following example retrieves value of the `AVMetadataItem` `.commonKeyTitle` key whose value can safely be assumed to be `String`.
+     
+     ```
+     asset.commonMetadataPublisher(key: .commonKeyTitle, as: String.self)
+         .receive(on: DispatchQueue.main)
+         .sink { title in
+             print("the title is \(title)") // title is a String
+         }
+         .store(in: &subscriptions)
+     ```
+     - Parameter key: key of the `AVMetadataItem`
+     - Parameter as: the concrete type to cast the `AVMetadataItem`'s value to (if found)
+     - Returns: A `Publisher` that emits the value `AVMetadataItem` matching a given `AVMetadataKey` (if it exists in the `AVAsset`'s `commonMetadata` entries) casting said value to a given concrete type.
+     */
+    func commonMetadataPublisher<T>(key: AVMetadataKey, as: T.Type) -> AnyPublisher<T, Never> {
+        publisher(for: \.commonMetadata)
+            .compactMap { metadata -> T? in
+                metadata.filter { $0.commonKey == key }.first?.value as? T
             }
             .eraseToAnyPublisher()
     }
