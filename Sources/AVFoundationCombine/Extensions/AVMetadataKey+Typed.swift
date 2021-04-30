87f6a272bedcd55bdb2725f9b0c89bce0a674457
Juan Carlos Ospina Gonzalez <piterwilson@gmail.com>
2021-04-30 15:28:10 +0200

    feat: Adds `AVPlayerMetadataKey+Typed`

---
+++ Sources/AVFoundationCombine/Extensions/AVMetadataKey+Typed.swift
@@ -0,0 +1,67 @@
+//
+//  AVPlayerMetadataKey+Typed.swift
+//  AVFoundation-Combine
+//
+//  Created by Juan Carlos Ospina Gonzalez on 30/04/2021.
+//
+
+import Foundation
+import Combine
+import AVKit
+
+protocol AVMetadataKeyConvertible {
+    func toAVMetadataKey() -> AVMetadataKey
+}
+
+public extension AVMetadataKey {
+    
+    /// `AVMetadataKey` that can safely assume to have a `Data` value containing an image,  in an `AVAsset`'s `commonMetadata` entry.
+    enum UIImageValueCommonKey: AVMetadataKeyConvertible {
+        
+        case commonKeyArtwork
+        
+        func toAVMetadataKey() -> AVMetadataKey {
+            switch self {
+            case .commonKeyArtwork:
+                return .commonKeyArtwork
+            }
+        }
+    }
+    
+    /// `AVMetadataKey` that can safely assume to have a `String` value in an `AVAsset`'s `commonMetadata` entry.
+    enum StringValueCommonKey: AVMetadataKeyConvertible {
+        
+        case commonKeyAlbumName
+        case commonKeyArtist
+        case commonKeyAuthor
+        case commonKeyCreator
+        case commonKeyCopyrights
+        case commonKeyDescription
+        case commonKeyLanguage
+        case commonKeyPublisher
+        case commonKeyTitle
+        
+        func toAVMetadataKey() -> AVMetadataKey {
+            switch self {
+            case .commonKeyAlbumName:
+                return .commonKeyAlbumName
+            case .commonKeyArtist:
+                return .commonKeyArtist
+            case .commonKeyAuthor:
+                return .commonKeyAuthor
+            case .commonKeyCreator:
+                return .commonKeyCreator
+            case .commonKeyCopyrights:
+                return .commonKeyCopyrights
+            case .commonKeyDescription:
+                return .commonKeyDescription
+            case .commonKeyLanguage:
+                return .commonKeyLanguage
+            case .commonKeyPublisher:
+                return .commonKeyPublisher
+            case .commonKeyTitle:
+                return .commonKeyTitle
+            }
+        }
+    }
+}
